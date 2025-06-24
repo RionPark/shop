a6335556de631acaddcd97cd455b611ca7bf4df4
Rion Park <redfila@nate.com>
2025-06-24 10:32:56 +0900

    product reply

--- src/main/java/com/furit/shop/controller/ShopController.java
+++ src/main/java/com/furit/shop/controller/ShopController.java
@@ -1,12 +1,18 @@
 package com.furit.shop.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.furit.shop.service.ProductReplyService;
 import com.furit.shop.service.ProductService;
+import com.furit.shop.vo.ProductReplyVO;
 import com.furit.shop.vo.ProductVO;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,12 +25,20 @@ public class ShopController {
  */
 	@Autowired
 	private ProductService productService;
+	@Autowired
+	private ProductReplyService productReplyService;
 	
 	@GetMapping("/shop-detail")
-	public String goShopDetail(@RequestParam("piNum")int piNum, Model m	) {
-		log.info("piNum=>{}", piNum);
-		ProductVO product = productService.selectProduct(piNum);
-		m.addAttribute("product",product);
+	public String goShopDetail(Model m, @ModelAttribute ProductReplyVO productReply) {
+		m.addAttribute("product",productService.selectProduct(productReply.getPiNum()));
+		m.addAttribute("productReplys", productReplyService.selectProductReplyList(productReply));
 		return "views/shop-detail";
+	}
+	
+	public static void main(String[] args) {
+		List<String> strs = Arrays.asList(new String[]{"1","2","3"});
+		for(String str : strs) {
+			
+		}
 	}
 }
